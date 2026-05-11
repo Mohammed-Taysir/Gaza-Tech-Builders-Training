fc8e6e3d67ea08c5c16d3fe1e90d5069026026ba
Mohammed Taysir <medo-11223344@hotmail.com>
2026-05-11 09:38:25 +0300

    add changes

--- small_project/src/protected/ProtectedRouter.js
+++ small_project/src/protected/ProtectedRouter.js
@@ -1,13 +1,32 @@
 
 import React from 'react'
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { auth } from "../firebase/Config";
 
 function ProtectedRouter({children}) {
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
   if(!user)
-    return <Navigate to = "/signin" replace/>
+    return <div style = {{
+      width: "100%",
+      height: "100%",
+      display: 'flex',
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <p><button style = {{
+        border: "none",
+        color: "#1F2B8F",
+        fontWeight: "bold",
+        fontSize: "20px",
+        cursor: "pointer"
+      }}
+      onClick={() => {
+        navigate("/signin")
+      }}
+      >Sign In</button> to Continue</p>
+    </div>
   return (
     <>
     {children}
